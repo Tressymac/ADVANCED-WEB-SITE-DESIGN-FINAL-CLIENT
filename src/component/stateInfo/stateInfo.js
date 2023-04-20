7463acc4fe9e6408ceb0072fed75fcdd20ac4f64
Tessica Mac <jessicamac48@yahoo.com>
2023-04-20 14:47:23 -0400

    styling

--- src/component/stateInfo/stateInfo.js
+++ src/component/stateInfo/stateInfo.js
@@ -3,7 +3,6 @@ import '../stateInfo/stateInfo.css'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 function StateInfo( {} ){
     const [returnedState, setStates] = useState([]);
 
@@ -23,21 +22,12 @@ function StateInfo( {} ){
         loadStates();
     }, []);
     // console.log(returnedState.map(element => (element.name)));
-
-      
+ 
     return(
-        // <div className="card" style={{width: 18 + 'rem'}}>
-        //     <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-        //     <div className="card-body">
-        //         <h5 className="card-title">Card title</h5>
-        //         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         <a href="#" className="btn btn-primary">Go somewhere</a>
-        //     </div>
-        // </div>
-        <div className="row">
+        <div className="row stateContainer">
         {returnedState.map(state => (
-            <div key={state._id} className="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div className="card">
+            <div key={state._id} className="col-sm-6 col-md-4 col-lg-3 mb-3 boxStates">
+            <div className="card cardItem">
                 <img src={state.image} className="card-img-top" alt={state.name} />
                 <div className="card-body">
                     <h5 className="card-title">{state.name}</h5>
@@ -48,7 +38,6 @@ function StateInfo( {} ){
             </div>
         ))}
         </div>
-
     )
 }
 
