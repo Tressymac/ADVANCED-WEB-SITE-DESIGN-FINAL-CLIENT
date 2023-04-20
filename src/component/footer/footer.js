c27dad3b8455f91a787d1816450abe066ee4ec4d
Tessica Mac <jessicamac48@yahoo.com>
2023-04-20 15:39:53 -0400

    fixing footer

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -5,12 +5,12 @@ function Footer( {logo} ){
     return(
         <div className="card">
             <div className="card-header">
-                Featured
+                Thank you for view my webpage
             </div>
             <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">This Page was created by Tressica Mac @ 2023</h5>
+                <p className="card-text">Click below to learn more about US States</p>
+                <a href="https://en.wikipedia.org/wiki/U.S._state" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     )
